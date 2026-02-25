3bf7a20a401f2fb8e58dd1c986d7a65f86ffd519
pjblecha <44296698+pjblecha@users.noreply.github.com>
2026-02-25 09:27:29 -0600

    there, I fixed it

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('data/db.json');
 const middleware = jsonServer.defaults();
+
 const port = process.env.PORT || 13579;
 
 server.use(middleware);
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}...`);
+});
