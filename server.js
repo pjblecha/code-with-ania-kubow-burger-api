43d59814cb34fafebe4933b0b7e30a9b12d88f99
pjblecha <44296698+pjblecha@users.noreply.github.com>
2026-02-25 08:49:02 -0600

    updating json and adding server backend

---
+++ server.js
@@ -0,0 +1,10 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('data/db.json');
+const middleware = jsonServer.defaults();
+const port = process.env.PORT || 13579;
+
+server.use(middleware);
+server.use(router);
+
+server.listen(port);
